5f53a49d74e31017bdf590121bb31c34708d9a2c
transcental <amber@transcental.dev>
2025-11-04 08:34:58 +0000

    allow ws admins to bypass channel manager check

--- slack_extra/utils/slack.py
+++ slack_extra/utils/slack.py
@@ -6,6 +6,13 @@ async def is_channel_manager(user_id: str, channel_id: str):
 
     data = {"token": config.slack.xoxc_token, "entity_id": channel_id}
     headers = {"Cookie": f"d={config.slack.xoxd_token}"}
+    user_data = await env.slack_client.users_info(user=user_id)
+    user = user_data.get("user", {})
+    is_admin = user.get("is_admin")
+    is_owner = user.get("is_owner")
+    is_primary_owner = user.get("is_primary_owner")
+    if is_admin or is_owner or is_primary_owner:
+        return True
 
     async with env.http.post(
         "https://slack.com/api/admin.roles.entity.listAssignments",
