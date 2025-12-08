01336463cfaf80f55763247ca9429edce5726d09
transcental <amber@transcental.dev>
2025-12-08 05:50:41 +0000

    remove anchor call from channel_created event

--- slack_extra/events/channel_created/__init__.py
+++ slack_extra/events/channel_created/__init__.py
@@ -2,7 +2,6 @@ from slack_bolt.context.ack.async_ack import AsyncAck
 from slack_sdk.web.async_client import AsyncWebClient
 
 from slack_extra.events.channel_created.join_channel import join_channel_handler
-from slack_extra.events.message.anchor import anchor_message_handler
 
 
 async def channel_created_handler(
@@ -10,5 +9,4 @@ async def channel_created_handler(
 ):
     await ack()
 
-    await anchor_message_handler(body, event, client)
     await join_channel_handler(body, event, client)
