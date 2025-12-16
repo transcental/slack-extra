09053ae9db85528ceb7c6697fd117d7e1ca3e9a7
transcental <amber@transcental.dev>
2025-12-16 00:32:48 +0000

    remove accidentally committed reactions initialisers

--- slack_extra/views/__init__.py
+++ slack_extra/views/__init__.py
@@ -1,14 +1,14 @@
 from slack_extra.views.configure_anchor import configure_anchor_handler
 from slack_extra.views.create_spoiler import create_spoiler_handler
+from slack_extra.views.edit_move import edit_move_handler
 from slack_extra.views.setup_move import setup_move_handler
-# from slack_extra.views.move_menu import move_menu_handler
 
 
 VIEWS = [
     {"id": "configure_anchor", "handler": configure_anchor_handler},
     {"id": "create_spoiler", "handler": create_spoiler_handler},
-    # {"id": "move_menu", "handler": move_menu_handler},
     {"id": "setup_move", "handler": setup_move_handler},
+    {"id": "edit_move", "handler": edit_move_handler},
 ]
 
 
